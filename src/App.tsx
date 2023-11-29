b53d424a67e9c2ccfefd9cb058f226751755aea9
Mariano <marianoaguirre14@gmail.com>
2023-11-29 17:15:07 -0300

    Header 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import './App.css';
-
-interface MiBotonProps{
-  contenido:string
-}
-
-function MiBoton(props:MiBotonProps){
-  return (
-    <button className="font-medium bg-red-900 p-3 rounded uppercase " type="button">
-      {props.contenido}
-    </button>
-  )
-}
+import { Header } from './components/Header';
 
 export default function App() {
   return (
-    <div className="App flex justify-center items-center h-screen">
-      <MiBoton contenido={"Hello there!"} />
+    <div>
+       <Header/>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,12 +7,22 @@ export type HeaderProps = {
 
 const Header: React.FC<HeaderProps>  = ({}) => {
 	return (
-		<header>
- 			<button type="button">Conoce mas</button>
-			<p>DESCRIPCION</p>
-			<img src='' alt='fondo tech team'></img>
- 		</header>
-	);
-};
+	
+			<header className='w-full absolute inset-0'>
+			  <img
+				src='https://assets.glginsights.com/wp-content/uploads/2021/08/D1_Tech_HeaderImage.jpg'
+				alt='fondo tech team'
+				className='w-full h-full object-cover absolute inset-0'
+			  />
+		
+			  <div className='absolute inset-0 flex flex-col items-center justify-center text-white text-center'>
+				<h1 className='text-9xl'>TECH TEAM</h1>
+				<button type="button" className='bg-blue-500 text-white px-4 py-2 mt-4'>
+				  Conoce más
+				</button>
+			  </div>
+			</header>
+		  );
+  };
 
 export default Header;
