0a9a639808c2795d01a1d53f65764aa568a94ae2
elianbenjamin <elianbenjamin30@gmail.com>
2023-11-29 15:59:43 -0300

    components

---
+++ src/components/About/About.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+
+export type AboutProps = {
+	// types...
+}
+
+const About: React.FC<AboutProps>  = ({}) => {
+	return (
+		<div>
+ 			About works!
+ 		</div>
+	);
+};
+
+export default About;
